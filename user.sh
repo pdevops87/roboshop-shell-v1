e73e01c652be2cc651b3386a57d14542fecc3149
Your Name <vemasani.pavani@gmail.com>
2025-11-20 14:45:22 +0530

    added user service

--- user.sh
+++ user.sh
@@ -4,13 +4,16 @@ dnf module enable nodejs:20 -y
 cp user.service /etc/systemd/system/user.service
 
 dnf install nodejs -y
+
 useradd roboshop
 rm -rf /app
 mkdir /app
+
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
 cd /app
 
 unzip /tmp/user.zip
+
 cd /app
 npm install
 
