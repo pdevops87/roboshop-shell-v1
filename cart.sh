16568a7e37d075f74a0be5eacdd266be5b2d2fa3
Your Name <vemasani.pavani@gmail.com>
2025-11-20 13:44:39 +0530

    microservice cart

--- cart.sh
+++ cart.sh
@@ -8,6 +8,10 @@ mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cd /app
 unzip /tmp/cart.zip
+
+cd /app
+npm install
+
 systemctl daemon-reload
 systemctl enable cart
 systemctl restart cart
