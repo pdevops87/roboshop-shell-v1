0d1113d40f746a1f4f4b7c753918697f73ee788e
Your Name <vemasani.pavani@gmail.com>
2025-11-20 15:08:48 +0530

    added shipping microservice

--- shipping.sh
+++ shipping.sh
@@ -8,3 +8,12 @@ unzip /tmp/shipping.zip
 cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
+dnf install mysql -y
+
+mysql -h 172.31.31.68 -uroot -pRoboShop@1 < /app/db/schema.sql
+mysql -h 172.31.31.68 -uroot -pRoboShop@1 < /app/db/app-user.sql
+mysql -h 172.31.31.68 -uroot -pRoboShop@1 < /app/db/master-data.sql
+
+systemctl daemon-reload
+systemctl enable shipping
+systemctl restart shipping
