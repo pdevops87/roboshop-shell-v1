b14d961826e71056b419225a4de4203e7e70a9c3
Your Name <vemasani.pavani@gmail.com>
2025-11-20 15:22:39 +0530

    added shipping microservice

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,4 @@
 dnf install mysql-server -y
+mysql_secure_installation --set-root-pass RoboShop@1
 systemctl enable mysqld
 systemctl restart mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
