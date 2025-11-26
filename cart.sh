011dec934838cdc118c2ca15724fd5e77e7a8ce3
Your Name <vemasani.pavani@gmail.com>
2025-11-26 10:58:48 +0530

    host changed

--- cart.sh
+++ cart.sh
@@ -1,10 +1,9 @@
 
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
-
-cp cart.service /etc/systemd/system/cart.service
 dnf install nodejs -y
 useradd roboshop
+cp cart.service /etc/systemd/system/cart.service
 mkdir /app
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cd /app
