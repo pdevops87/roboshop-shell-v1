5ceb63c3d8859fdb11d7a359d3b2e4b77e8bd13a
Your Name <vemasani.pavani@gmail.com>
2025-11-26 10:44:55 +0530

    user service

--- cart.sh
+++ cart.sh
@@ -1,3 +1,4 @@
+
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 
--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,13 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-org -y
+echo -e "\\e[33m copy mongo repo\\e[0m"
+echo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf
+echo -e "\\e[33m install mongo\\e[0m"
+echo
+dnf install mongodb-org -y &>>/tmp/roboshop.log
 
-systemctl enable mongod
-systemctl restart mongod
+echo -e "\\update address\\e[0m"
+sed -i -e 's/127.0.0.1/0.0.0.0/'  /etc/mongod.conf &>>/tmp/roboshop.log
+
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
