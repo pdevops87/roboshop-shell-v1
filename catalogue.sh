d43fffb14d5c286c243bb92f1010fbabe9230635
Your Name <vemasani.pavani@gmail.com>
2025-11-26 10:05:12 +0530

    add headers in common.sh

--- catalogue.sh
+++ catalogue.sh
@@ -1,13 +1,8 @@
+source ./common.sh
 component=catalogue
-
-echo -e "\\e[33m copy catalogue service"
 cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
-
-echo -e "\\e[33m copy mongo repo file \\e[0m"
-echo
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
-source ./common.sh
 nodejs
 mongodb
 systemd_Service
