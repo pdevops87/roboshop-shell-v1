ec2d3b5aa2ee28b3b2137b2335d7d54969f8310b
Your Name <vemasani.pavani@gmail.com>
2025-11-26 10:18:55 +0530

    add header and exit status

--- catalogue.sh
+++ catalogue.sh
@@ -1,8 +1,21 @@
 source ./common.sh
 component=catalogue
+echo -e "\\e[33m Copy catalogue service file \\e[0m"
+echo
 cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+
+echo -e "\\e[33m Copy mongo repo file \\e[0m"
+echo
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
+echo -e "\\e[33m run nodejs \\e[0m"
+echo
 nodejs
+
+echo -e "\\e[33m run mongodb \\e[0m"
+echo
 mongodb
+
+echo -e "\\e[33m run systemd service\\e[0m"
+echo
 system_Service
--- common.sh
+++ common.sh
@@ -1,20 +1,37 @@
 nodejs(){
+echo -e "\\e[33m disable nodejs \\e[0m"
 dnf module disable nodejs -y &>>/tmp/roboshop.log
+echo $?
+echo -e "\\e[33m enable nodejs \\e[0m"
 dnf module enable nodejs:20 -y &>>/tmp/roboshop.log
+echo $?
+echo -e "\\e[33m install nodejs \\e[0m"
 dnf install nodejs -y &>>/tmp/roboshop.log
+echo $?
+echo -e "\\e[33m Add roboshop user \\e[0m"
 useradd roboshop &>>/tmp/roboshop.log
+echo $?
+echo -e "\\e[33m make a directory \\e[0m"
 mkdir /app &>>/tmp/roboshop.log
+echo $?
+echo -e "\\e[33m download content \\e[0m"
 curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>/tmp/roboshop.log
+echo $?
+echo -e "\\e[33m navigate to /app \\e[0m"
 cd /app &>>/tmp/roboshop.log
+echo $?
+echo -e "\\e[33m unarchive content \\e[0m"
 unzip /tmp/$component.zip &>>/tmp/roboshop.log
-echo -e "\\e[33m navigate to /app directory \\e[0m"
-echo
+echo $?
+echo -e "\\e[33m navigate to /app  \\e[0m"
 cd /app &>>/tmp/roboshop.log
+echo $?
 echo -e "\\e[33m npm install \\e[0m"
-echo
 npm install &>>/tmp/roboshop.log
+echo $?
 }
 system_Service(){
+  echo -e "\\e[33m component service \\e[0m"
   systemctl daemon-reload &>>/tmp/roboshop.log
   systemctl enable $component &>>/tmp/roboshop.log
   systemctl start $component &>>/tmp/roboshop.log
