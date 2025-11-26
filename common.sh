edf3c3c13460b1314835b263b1cdfcd524ef81cb
Your Name <vemasani.pavani@gmail.com>
2025-11-26 09:38:36 +0530

    avoid duplicate code

---
+++ common.sh
@@ -0,0 +1,27 @@
+nodejs(){
+dnf module disable nodejs -y &>>/tmp/roboshop.log
+dnf module enable nodejs:20 -y &>>/tmp/roboshop.log
+dnf install nodejs -y &>>/tmp/roboshop.log
+useradd roboshop &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip
+cd /app &>>/tmp/roboshop.log
+npm install &>>/tmp/roboshop.log
+}
+system_Service(){
+  systemctl daemon-reload &>>/tmp/roboshop.log
+  systemctl enable $component &>>/tmp/roboshop.log
+  systemctl start $component &>>/tmp/roboshop.log
+}
+
+mongodb(){
+  echo -e "\\e[33m install mongodb  \\e[0m"
+  echo
+  dnf install mongodb-mongosh -y &>>/tmp/roboshop.log
+
+  echo -e "\\e[33m load master-data \\e[0m"
+  echo
+  mongosh --host mondodb-dev.pdevops78.online </app/db/master-data.js &>>/tmp/roboshop.log
+}
